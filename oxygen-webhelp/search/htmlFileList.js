a52812881ada0f1bf483eb74a1acb350b5ee42ac
Rajani Gundimeda <gundimeda@ge.com>
2016-12-21 13:31:52 -0800

    deletin again

--- oxygen-webhelp/search/htmlFileList.js
+++
@@ -1,9 +0,0 @@
-//List of files which are indexed.
-fl = new Array();
-fl["0"]= "Chunk1216278508.html";
-fl["1"]= "Chunk1789227346.html";
-fl["2"]= "Chunk991911554.html";
-fl["3"]= "c_overview_of_intelligent_enterprises_apis.html";
-fl["4"]= "r_enterprise_environment_api.html";
-fl["5"]= "r_indoor_positioning_api-1.html";
-var doStem = false;searchLoaded = true;
